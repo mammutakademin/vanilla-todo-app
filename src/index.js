1de13ce109445af08e81b9804698e44077a545b6
David Lundholm <david.lundholm@zocom.se>
2022-01-04 11:21:51 +0100

    Add delete capability

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import './styles/main.scss'
 const todos = []
 //Todo Model
 // {
+//   id: String,
 //   content: String,
 //   done: Boolean
 // }
 
+function generateID(){
+  return String(Date.now()) + String(Math.ceil(Math.random()*Math.pow(10,5)))
+}
+
 function addTodo(content){
   const todo = {
+    id: generateID(),
     content: content,
     done: false
   }
+  console.log(todo.id);
   todos.push(todo)
   return todo
 }
@@ -36,10 +43,20 @@ function renderTodo(todo){
   const li = document.createElement("li")
   li.classList.add("todo-item")
   li.innerHTML = todoTemplate(todo)
+
   li.addEventListener("click", () => {
     todo.done = !todo.done
     li.classList.toggle("done")
   })
+
+  li.querySelector(".edit").addEventListener("click", () => {})
+  li.querySelector(".delete").addEventListener("click", () => {
+    const index = todos.findIndex(t => t.id == todo.id)
+    todos.splice(index, 1)
+    li.remove()
+  })
+
+
   todoList.append(li)
 
 }
