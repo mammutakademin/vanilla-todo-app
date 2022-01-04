419a9083acdb732cbd4b55643c7a4a9c98bbd6be
David Lundholm <david.lundholm@zocom.se>
2022-01-04 11:39:24 +0100

    Add todos left feature. Fix bug with done check

--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ function loadState(){
   }
 }
 
+function todosLeft(){
+  let sum = 0
+  for(let todo of todos){
+    sum += todo.done ? 0 : 1
+  }
+  return sum
+  // return todos.reduce((acc, current) => 
+  //   acc + todo.done ? 0 : 1, 0
+  // )
+}
 
 function generateID(){
   return String(Date.now()) + String(Math.ceil(Math.random()*Math.pow(10,5)))
@@ -50,17 +60,26 @@ function todoTemplate(todo){
 }
 
 function renderTodo(todo){
+  console.log(todos);
   const todoList = document.querySelector(".todo-list")
   const li = document.createElement("li")
   li.classList.add("todo-item")
+  if(todo.done){
+    li.classList.add("done")
+  }
   li.innerHTML = todoTemplate(todo)
 
   li.addEventListener("click", () => {
     todo.done = !todo.done
     li.classList.toggle("done")
+    renderTodosLeft()
+    persist()
   })
 
-  li.querySelector(".edit").addEventListener("click", () => {})
+  li.querySelector(".edit").addEventListener("click", () => {
+
+  })
+
   li.querySelector(".delete").addEventListener("click", () => {
     const index = todos.findIndex(t => t.id === todo.id)
     todos.splice(index, 1)
@@ -68,16 +87,19 @@ function renderTodo(todo){
     persist()
   })
 
-
   todoList.append(li)
+}
 
+function renderTodosLeft(){
+  const p = document.querySelector(".todos-left strong")
+  p.innerText = todosLeft()
 }
 
 function addTodoController(){
   const todoInput = document.querySelector(".todo-input")
   const todo = addTodo(todoInput.value)
   renderTodo(todo)
-  console.log(todos)
+  renderTodosLeft()
 }
 
 function main(){
